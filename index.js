2e27dc6254142a374b0e6e5a5203d8aff388cfe7
Joe Mullen <mullenjoe13@gmail.com>
2018-10-16 22:02:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,10 +22,10 @@ sayHiToGrandma('HELLO!');
 
 
 function sayHiToGrandma(string) {
-  if (string = lowercase)
+  if (string = lowercase);
   return "I can't hear you!"
- else if (string = uppercase)
+ else if (string = uppercase);
   return "YES INDEED!"
- else if  (string = mixedCase)
+ else if  (string = mixedCase);
   return "I love you, too."
 };
