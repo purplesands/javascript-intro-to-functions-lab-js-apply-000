1ed18fba6e171442a5c605eaa77f342895f93e13
Joe Mullen <mullenjoe13@gmail.com>
2018-11-28 18:59:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,5 +36,11 @@ function sayHiToGrandma(string) {
     return "YES INDEED!";
 } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
+} if (string = lowercase) {
+  return "I can't hear you!";
+} else if (string = uppercase) {
+  return "YES INDEED!";
+} else if (string = "I love you, Grandma.") {
+  return "I love you, too.";
 }
-};
+}
